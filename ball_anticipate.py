1267418447dc10bba8be7d3dd18ec003c29eecfe
hia <1009304154@qq.com>
2019-02-23 09:34:27 +0800

    更新了随机算法

--- ball_anticipate.py
+++ ball_anticipate.py
@@ -10,24 +10,33 @@ def create_number_list():
     anticipate_blue = 0  # 预测出的篮球
     # 抽6个红球
     for i in range(0, 6):
-        time.sleep(random.uniform(0, 0.73))  # 随机等待时间（随机时间执行摇出不同个球）
+        time.sleep(random.uniform(0, 0.0055))  # 随机等待时间（随机时间执行摇出不同个球）
         anticipate_red.append(red_ball.pop(random.choice(range(0, 33 - i))))  # 记录随机抽出的红球
 
     anticipate_red.sort()  # 顺序
     # anticipate_red.reverse()  # 倒序
-
     anticipate_blue = blue_ball.pop(random.choice(range(0, 16)))  # 记录随机抽出的蓝球
 
-    print('红球号码:{}'.format(anticipate_red))
-    print('篮球号码:{}\n'.format(anticipate_blue))
+    return anticipate_red, anticipate_blue
+
+    # print('红球号码:{}'.format(anticipate_red))
+    # print('篮球号码:{}\n'.format(anticipate_blue))
 
 def main():
     print('双色球摇号中')
     create_count = 4  # 预测4组
-    for i in range(0, 4):
-        time.sleep(random.uniform(0, 3.14159))
-        create_number_list()
-    print('随机模拟结束')
+    all_count = random.choice(range(100, 200))
+    ball_group = []
+    anwser_group = []
+    for i in range(0, all_count):
+        time.sleep(random.uniform(0, 0.00314159))
+        ball_group.append(create_number_list())
+    for i in range(0, create_count):
+        time.sleep(random.uniform(0, 0.00055))  # 随机等待时间（随机时间执行摇出不同个球）
+        anwser_group.append(ball_group.pop(random.choice(range(0, all_count - i))))  # 记录随机抽出的红球
+    for i in anwser_group:
+        print('红球号码：{}'.format(i[0]), '   篮球号码：{}'.format(i[1]))
+
 
 if __name__ == '__main__':
     main()
--- great_lotto.py
+++ great_lotto.py
@@ -10,27 +10,35 @@ def create_number_list():
     anticipate_blue = []  # 预测出的篮球
     # 抽6个红球
     for i in range(0, 5):
-        time.sleep(random.uniform(0, 0.73))  # 随机等待时间（随机时间执行摇出不同个球）
+        time.sleep(random.uniform(0, 0.00055))  # 随机等待时间（随机时间执行摇出不同个球）
         anticipate_red.append(red_ball.pop(random.choice(range(0, 35 - i))))  # 记录随机抽出的红球
 
     anticipate_red.sort()  # 顺序
     # anticipate_red.reverse()  # 倒序
     for i in range(0, 2):
-        time.sleep(random.uniform(0, 0.73))  # 随机等待时间（随机时间执行摇出不同个球）
+        time.sleep(random.uniform(0, 0.00056))  # 随机等待时间（随机时间执行摇出不同个球）
         anticipate_blue.append(blue_ball.pop(random.choice(range(0, 12 - i))))  # 记录随机抽出的蓝球
 
     anticipate_blue.sort()  # 顺序
 
-    print('红球号码:{}'.format(anticipate_red))
-    print('篮球号码:{}\n'.format(anticipate_blue))
+    return anticipate_red, anticipate_blue
+
 
 def main():
     print('大乐透摇号中')
     create_count = 4  # 预测4组
-    for i in range(0, 4):
-        time.sleep(random.uniform(0, 3.14159))
-        create_number_list()
-    print('随机模拟结束')
+    all_count = random.choice(range(100, 200))
+    ball_group = []
+    anwser_group = []
+    for i in range(0, all_count):
+        time.sleep(random.uniform(0, 0.00314159))
+        ball_group.append(create_number_list())
+    for i in range(0, create_count):
+        time.sleep(random.uniform(0, 0.00055))  # 随机等待时间（随机时间执行摇出不同个球）
+        anwser_group.append(ball_group.pop(random.choice(range(0, all_count - i))))  # 记录随机抽出的红球
+    for i in anwser_group:
+        print('前区号码：{}'.format(i[0]), '   后区号码：{}'.format(i[1]))
+        # print()
 
 if __name__ == '__main__':
     main()
